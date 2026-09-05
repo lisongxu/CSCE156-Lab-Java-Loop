67d914eb84a6ceecf7023dcb3c923fa473f5de07
Lisong Xu <xu@unl.edu>
2026-09-05 15:02:02 -0500

    Update import statements in NaturalTests.java

--- src/test/java/unl/soc/NaturalTests.java
+++ src/test/java/unl/soc/NaturalTests.java
@@ -1,10 +1,9 @@
 package unl.soc;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import unl.soc.Natural;
-
-import org.junit.Test;
 
 public class NaturalTests {
 	 
